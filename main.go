2662d4a6dae9ec68c29631f8f67e7e7ae7e9388e
Vincent Chapron <vincent@chapron.io>
2016-09-07 17:38:27 +0200

    add logs

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func main() {
 
     iris.Use(cors.Default())
 	iris.Post("/update/statistics", func (ctx *iris.Context) {
+        fmt.Println("ON POST")
         if err := ctx.ReadJSON(&_promotion); err != nil {
             ctx.JSON(400, map[string]string {"success": "false"})
         } else {
@@ -37,12 +38,14 @@ func main() {
 
 	iris.Config.Websocket.Endpoint = "/websocket"
 	iris.Websocket.OnConnection(func (c iris.WebsocketConnection) {
+        fmt.Println("ON CONNECTION")
         c.Join("statistics")
 
 		continu := true
         go func() {
             for continu {
         		<-update
+                fmt.Println("ON UPDATE")
                 emit := fmt.Sprintf("statistics_%s", _promotion.Id)
                 c.To("statistics").Emit(emit, _statistics)
                 _promotion = Promotion{}
