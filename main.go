2f51b89bc7f3fb576f9665ac006795492fb7723a
Vincent Chapron <vincent@chapron.io>
2016-09-07 14:23:15 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import "github.com/kataras/iris"
+import "fmt"
+
+type Promotion struct {
+    Id      string  `json:"id"`
+}
+
+type Statistics struct {
+    Total   int     `json:"total"`
+    Present int     `json:"present"`
+    Absent  int     `json:"absent"`
+    Late    int     `json:"late"`
+    Waiting int     `json:"waiting"`
+}
+
+func main() {
+	update := make(chan string)
+    _promotion := Promotion{}
+    _statistics := Statistics{}
+
+	iris.Post("/update/statistics", func (ctx *iris.Context) {
+        if err := ctx.ReadJSON(&_promotion); err != nil {
+            ctx.JSON(400, map[string]string {"success": "false"})
+        } else {
+            if err := ctx.ReadJSON(&_statistics); err != nil {
+                ctx.JSON(400, map[string]string {"success": "false"})
+            } else {
+                go func() {update <- "statistics"}()
+        		ctx.JSON(200, map[string]string {"success": "true"})
+            }
+        }
+	})
+
+	iris.Config.Websocket.Endpoint = "/websocket"
+	iris.Websocket.OnConnection(func (c iris.WebsocketConnection) {
+        c.Join("statistics")
+
+		continu := true
+        go func() {
+            for continu {
+        		<-update
+                emit := fmt.Sprintf("statistics_%s", _promotion.Id)
+                c.To("statistics").Emit(emit, _statistics)
+                _promotion = Promotion{}
+                _statistics = Statistics{}
+        	}
+    	}()
+
+		c.OnDisconnect(func() {
+			continu = false
+		})
+	})
+
+	iris.Listen("127.0.0.1:8001")
+}
